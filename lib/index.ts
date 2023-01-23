5e5bbd9fab3bb6dcca44b41b7d3b3db1a33a3023
WhiteAngel13 <edmilsonsw22@gmail.com>
2023-01-23 13:23:16 -0400

    feat: create RedisCLI class

--- lib/index.ts
+++ lib/index.ts
@@ -6,18 +6,35 @@ type ConnectionConfig = {
   host: string;
   options?: ClientOpts;
 }
-export class ReliableQueue<T> {
+
+class RedisCli {
   private redisCli: WrappedNodeRedisClient | undefined;
 
   constructor(
-    private readonly name: string,
     private readonly connectionConfig: ConnectionConfig,
   ) {}
 
-  async getRedisConnection(): Promise<WrappedNodeRedisClient> {
+  private async newConnection(): Promise<WrappedNodeRedisClient> {
+    const {port, host, options} = this.connectionConfig;
+    const redisCli = createNodeRedisClient(port, host, options);
+    
+    if(options && options.password) {
+      // auth may be fullfilled or rejected
+      const auth = await redisCli.auth(options.password);
+
+      if(auth === "OK") {
+        return redisCli;
+      }
+    }
+    
+    throw new Error("Could not connect to redis server because password is not set");
+  }
+
+  private async getRedisConnection(): Promise<WrappedNodeRedisClient> {
     if(this.redisCli) {
       return this.redisCli;
     }
+
     const {port, host, options} = this.connectionConfig;
     
     const redisCli = createNodeRedisClient(port, host, options);
@@ -32,6 +49,41 @@ export class ReliableQueue<T> {
     }
 
     throw new Error("Could not connect to redis server because password is not set");
+  }
+
+  private async assertRedisConnection(): Promise<void> {
+    const redisCli = await this.getRedisConnection();
+
+    try{
+      await redisCli.ping();
+    }catch(e) {
+      try{
+        this.redisCli = await this.newConnection();
+      }catch(e){
+        throw new Error("Could not connect to redis server");
+      }
+    }
+  }
+
+  async getCli(): Promise<WrappedNodeRedisClient> {
+    await this.assertRedisConnection();
+    return await this.getRedisConnection();
+  }
+
+  async close(): Promise<void> {
+    const redisCli = await this.getRedisConnection();
+    await redisCli.quit();
+  }
+}
+
+export class ReliableQueue<T> {
+  private redisCli: RedisCli
+
+  constructor(
+    private readonly name: string,
+    private readonly connectionConfig: ConnectionConfig,
+  ) {
+    this.redisCli = new RedisCli(connectionConfig);
   }
 
   static newWithRedisOpts<T2>(
@@ -50,14 +102,13 @@ export class ReliableQueue<T> {
   }
 
   async listen(workerID: string): Promise<Listener<T>> {
-    const redisCli = await this.getRedisConnection();
     const processingQueue = this.name + "-processing-" + workerID;
     await this.queuePastEvents(processingQueue);
     return new Listener<T>(this.name, processingQueue, this.connectionConfig);
   }
 
   async pushMessage(topic: string, content: T): Promise<number> {
-    const redisCli = await this.getRedisConnection();
+    const redisCli = await this.redisCli.getCli()
 
     const n = await redisCli.lpush(
       this.name,
@@ -67,20 +118,19 @@ export class ReliableQueue<T> {
   }
 
   async pushRawMessage(content: T): Promise<number> {
-    const redisCli = await this.getRedisConnection();
+    const redisCli = await this.redisCli.getCli()
 
     const n = await redisCli.lpush(this.name, JSON.stringify(content));
     return n;
   }
 
   async close(): Promise<void> {
-    const redisCli = await this.getRedisConnection();
+    const redisCli = await this.redisCli.getCli();
     await redisCli.quit();
   }
 
   private async queuePastEvents(processingQueue: string): Promise<void> {
-    const redisCli = await this.getRedisConnection();
-
+    const redisCli = await this.redisCli.getCli();
     let lastv = "init";
     while (lastv) {
       lastv = await redisCli.rpoplpush(processingQueue, this.name);
@@ -89,19 +139,20 @@ export class ReliableQueue<T> {
 }
 
 export class Listener<T> {
-  private redisCli: WrappedNodeRedisClient | undefined;
+  private redisCli: RedisCli
 
   constructor(
     private readonly name: string,
     private readonly processingQueue: string,
-    private readonly connectionConfig: ConnectionConfig,
-  ) {}
+    connectionConfig: ConnectionConfig,
+  ) {
+    this.redisCli = new RedisCli(connectionConfig);
+  }
 
   async waitForMessage(
     timeout: number = 10
   ): Promise<[Message<T> | null, Function]> {
-    await this.assertRedisConnection();
-    const redisCli = await this.getRedisConnection();
+    const redisCli = await this.redisCli.getCli();
     
     const msg = await redisCli.brpoplpush(
       this.name,
@@ -118,45 +169,6 @@ export class Listener<T> {
       await redisCli.lrem(this.processingQueue, 1, msg);
     };
     return [v, afn];
-  }
-
-  private async getRedisConnection(): Promise<WrappedNodeRedisClient> {
-    if(this.redisCli) {
-      return this.redisCli;
-    }
-    
-    this.redisCli = await this.newConnection();
-    return this.redisCli;
-  }
-
-  private async newConnection(): Promise<WrappedNodeRedisClient> {
-    const {port, host, options} = this.connectionConfig;
-    const redisCli = createNodeRedisClient(port, host, options);
-    
-    if(options && options.password) {
-      // auth may be fullfilled or rejected
-      const auth = await redisCli.auth(options.password);
-
-      if(auth === "OK") {
-        return redisCli;
-      }
-    }
-    
-    throw new Error("Could not connect to redis server because password is not set");
-  }
-
-  private async assertRedisConnection(): Promise<void> {
-    const redisCli = await this.getRedisConnection();
-
-    try{
-      await redisCli.ping();
-    }catch(e) {
-      try{
-        this.redisCli = await this.newConnection();
-      }catch(e){
-        throw new Error("Could not connect to redis server");
-      }
-    }
   }
 }
 
